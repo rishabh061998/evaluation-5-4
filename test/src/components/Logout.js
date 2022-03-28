d37e63f450eee6bc1dfc63b91dd18e01112aaf71
rishabh061998 <rishabhjune06.1998@gmail.com>
2022-03-28 15:34:07 +0530

    5

--- test/src/components/Logout.js
+++ test/src/components/Logout.js
@@ -1,10 +1,22 @@
 import React from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { logout, selectUser } from "../features/userSlice"
 
 const Logout=()=>{
+
+    const user=useSelector(selectUser)
+    const dispatch=useDispatch()
+const handleLogout=(e)=>{
+    e.preventDefault()
+
+   
+    dispatch(logout())
+}
+
     return(
         <div>
-             <h1>Welcome <span className="user_name">Rishabh</span></h1>
-             <button className="logout_button">Logout</button>
+             <h1>Welcome <span className="user_name">{user.name}</span></h1>
+             <button className="logout_button" onClick={(e)=>handleLogout(e)}>Logout</button>
         </div>
     )
 }
