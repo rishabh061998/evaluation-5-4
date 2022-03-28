caae84110bacebed7de9745a3ac8e579124690d8
rishabh061998 <rishabhjune06.1998@gmail.com>
2022-03-28 15:14:25 +0530

    3

--- test/src/components/Login.js
+++ test/src/components/Login.js
@@ -1,13 +1,20 @@
-import React from "react"
+import React, { useState } from "react"
 
 const Login=()=>{
+
+    const[name,setName]=useState("")
+    const[email,setEmail]=useState("")
+    const[password,setPassword]=useState("")
+
     return(
         <div className="login">
             <form className="login_form">
              <h1>Login Here</h1>
-             <input type="name" placeholder="Name"></input>
-             <input type="name" placeholder="Location"></input>
-             <input type="name" placeholder="Interested-Topics"></input>
+             <input type="name" placeholder="Name" value={name} onChange={(e)=>setName(e.target.value)}></input>
+             <input type="email" placeholder="Location" value={email}onChange={(e)=>setEmail(e.target.value)}></input>
+             <input type="password" placeholder="Interested-Topics" value={password}onChange={(e)=>setPassword(e.target.value)}></input>
+
+             <button type="submit" className="submit_btn">submit</button>
             </form>
 
         </div>
---
+++ test/src/components/Logout.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Logout=()=>{
+    return(
+        <div>
+             <h1>Welcome <span className="user_name">Rishabh</span></h1>
+             <button className="logout_button">Logout</button>
+        </div>
+    )
+}
+
+export default Logout
