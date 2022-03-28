d92132ae390750ee49996a5479080ee88874160a
rishabh061998 <rishabhjune06.1998@gmail.com>
2022-03-28 14:42:24 +0530

    2

---
+++ test/src/components/Login.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Login=()=>{
+    return(
+        <div className="login">
+            <form className="login_form">
+             <h1>Login Here</h1>
+             <input type="name" placeholder="Name"></input>
+             <input type="name" placeholder="Location"></input>
+             <input type="name" placeholder="Interested-Topics"></input>
+            </form>
+
+        </div>
+    )
+}
+
+export default Login
